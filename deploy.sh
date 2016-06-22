39ef27e23c30d461da97bbb86a44d8689d8ae7b1
Psyko <contact@goko.fr>
2016-06-22 23:27:31 +0200

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for f in $(find output/)
+cd output
+
+for f in $(find /)
 do
   echo "$f"
   #curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/v0.3.0/$f
