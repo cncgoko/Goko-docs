17e051239a6a3d8234088ca18cf3b27dbfea3a06
cncgoko <cncgoko@users.noreply.github.com>
2016-06-23 07:38:56 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd output
 for f in $(find . -type f)
 do
   echo "$f"
-#  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/v0.3.0/$f
+  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/v0.3.0/$f
 done
