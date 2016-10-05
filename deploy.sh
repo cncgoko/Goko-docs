b9b62245b8338e7b798df9b5c6041a347018c29e
Psyko <contact@goko.fr>
2016-10-05 18:09:36 +0200

    Deploy tests

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ deleteFolders(){
 	for file in $(curl -s -l -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/$1); 
 	do
 	 echo "Removing file www/download/$1$file"
-	 curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/ -X "DELE $file"
+	 curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/$1 -X "DELE $file"
 	done
 	echo "Removing folder www/download/$1"
-	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/ -X "RMD $1"
+	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/$1 -X "RMD $1"
 	
 }
 
