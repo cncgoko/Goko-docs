e2fcdb553702779fd8790dab30ff926f153df98d
Psyko <contact@goko.fr>
2016-10-05 17:26:48 +0200

    Deploy tests

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+lftp -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/ mv master master_
+
 cd output
 
 for f in $(find . -type f)
 do
   echo "$f"
-  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/v0.3.1/$f
+  curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/master/$f
 done
