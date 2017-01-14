6070461f49d2c6c4c866056415200d7f93ebb225
Psyko <contact@goko.fr>
2017-01-14 12:26:29 +0100

    Version 0.3.2

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,22 @@
 deleteFolders(){
 	for file in $(curl -s -l -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr//www/docs/$1);
 	do
-	 echo "Removing file www/download/$1$file"
+	 echo "Removing file download/$1$file"
 	 curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/$1 -X "DELE $file"
 	done
-	echo "Removing folder www/download/$1"
+	echo "Removing folder download/$1"
 	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/$1 -X "RMD $1"
 }
 
 renameFolders(){
-	echo "Renaming folder www/docs/$1 to www/docs/$2"
-	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/$1 -X "RNFR $1"
-	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/$1 -X "RNTO $2"
+	echo "Renaming folder docs/$1 to docs/$2"
+	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/ -X "RNFR $1"
+	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/ -X "RNTO $2"
 }
 
-timestamp=`date +"%Y%m%d%H%M%S"`
-echo "Saving old docs to $previousDocVersion_$timestamp/"
-renameFolders master/ "$previousDocVersion_$timestamp/"
+timestamped=`date +"%Y%m%d%H%M%S"`
+echo "Saving old docs to $previousDocVersion_timestamped/"
+renameFolders master/ "$previousDocVersion_timestamped/"
 
 cd output
 
