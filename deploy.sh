4a9a569e4e0a917a682a1de7db3ca7c7572ee7b7
Psyko <contact@goko.fr>
2017-01-14 13:52:28 +0100

    Version 0.3.2

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,8 @@ deleteFolders(){
 
 renameFolders(){
 	echo "Renaming folder docs/$1 to docs/$2"
-	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/ -X "RNFR $1 RNTO $2"
+	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/ -X "RNFR $1"
+	curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/ -X "RNTO $2"
 	#curl -u $FTP_USER:$FTP_PASS ftp://ftp.goko.fr/www/docs/ -X "RNTO $2"
 }
 
